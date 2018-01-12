dd3005e2e775055c140758ef3127080aaabdb5c0
thubatilakshmi <thubatilakshmi@gmail.com>
2018-01-12 17:09:22 -0500

    some data manipulation

--- conv_to_csv.py
+++ conv_to_csv.py
@@ -1,9 +1,7 @@
-import numpy as np
 import pickle
 import csv
 
-import matplotlib.pyplot as plt
-from scipy.misc import imshow
+
 '''
 
 This file is to experiment and verify the code
@@ -20,27 +18,24 @@ def unpickle(fname):
 
 def main():
 
-    file_str = "data/training/train_data_batch_"
-    ls_fnames = [file_str+str(i) for i in range(1,11)]
-    fh = open("D:/training.csv", 'a', newline='')
+    file_str = "data/val_data"
+    fh_xdata = open("data/x_val.csv", 'a', newline='')
+    fh_ydata = open("data/y_val.csv",'a',newline = '')
 
-    for file_count in range(10):
+    wr_x = csv.writer(fh_xdata,dialect='excel')
+    wr_y = csv.writer(fh_ydata, dialect='excel')
+    val_data = unpickle(file_str)
+    print(val_data.keys())
+    size = len(val_data['labels'])
+    for i in range(size):
+        img_data = list(val_data['data'][i])
+        label = val_data['labels'][i]
+        wr_x.writerow(img_data)
+        wr_y.writerow([label])
+        print("\r Line: ".format(i)+str(i),end  = '')
 
-        wr = csv.writer(fh,dialect='excel')
-
-        val_data = unpickle(ls_fnames[file_count])
-        print(val_data.keys())
-        size = len(val_data['labels'])
-        for i in range(size):
-            img_data = list(val_data['data'][i])
-            label = val_data['labels'][i]
-            joined = [label]+img_data
-            wr.writerow(joined)
-            print("\r Line: ".format(i)+str(i),end  = '')
-        print("\n")
-        print("\r Finished File: ".format(file_count)+str(file_count),end = '')
-
-    fh.close()
+    fh_xdata.close()
+    fh_ydata.close()
 
 
 main()
--- data augmentation.py
+++ data augmentation.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 
 
 def rand_crop(img_data):
-    a = int(np.random.rand()*(64-52))
+    a = int(np.random.rand()*(32-24))
     ls_crop = []
     for c in range(0,3):
-        for i in range(a,a+52):
-            for j in range(a,a+52):
+        for i in range(a,a+24):
+            for j in range(a,a+24):
                 ls_crop.append(img_data[c][i][j])
 
-    img_crop = np.array(ls_crop).reshape(3,52,52)
+    img_crop = np.array(ls_crop).reshape(3,24,24)
     return img_crop
 
 
@@ -19,19 +19,19 @@ def mirror(img_data):
     for c in range(3):
         for i in range(len(img_data[c])):
             ls_mirror.append(list(reversed(img_data[c][i])))
-    img_mirror = np.array(ls_mirror).reshape(3,52,52)
+    img_mirror = np.array(ls_mirror).reshape(3,24,24)
     return img_mirror
 
 
 def main():
-    with open("data/validation.csv") as fh:
+    with open("data/x_val.csv") as fh:
         row = fh.readline()
         split_str = row.split(',')
         ls_img = []
-        for i in range(1, len(split_str)):
+        for i in range(len(split_str)):
             ls_img.append(int(split_str[i]))
 
-        img_data = np.array(ls_img).reshape(3, 64, 64)
+        img_data = np.array(ls_img).reshape(3, 32, 32)
 
     img_crop = rand_crop(img_data)
     img_mirror = mirror(img_crop)
