5037c9ff729dede5e8b3477a8d284c37e3971583
thubatilakshmi <thubatilakshmi@gmail.com>
2018-01-13 04:16:16 -0500

    all layers

--- Classifier.py
+++ Classifier.py
@@ -59,9 +59,47 @@ class Data():
                 y_data.append(row)
         self.y_data = np.array(y_data).astype(int)
 
+
+batch_size = 128
+
 gph = tf.Graph()
 with gph.as_default():
-    pass
+    x = tf.placeholder('float',shape = [None,24,24,3])
+    y_ = tf.placeholder('float',shape = [None,1000])
+
+    ls_ker_dims = [[3,3,3,96],
+                   [3,3,96,256],
+                   [3,3,256,384],
+                   [3,3,384,384],
+                   [3,3,384,256],
+                   [10*10*256,4096],
+                   [4096,4096],
+                   [4096,1000]]
+    # initializing all kernels and biases including fc layer
+    kernels = [0]*8
+    biases = []*8
+    for i in range(8):
+        kernels[i] = tf.Variable(tf.random_normal(shape = ls_ker_dims[i],mean = 0.0, stddev=0.01))
+        biases[i] = tf.Variable(1.0,shape = ls_ker_dims[i][-1])
+
+    amap = tf.nn.relu(tf.nn.conv2d(x,kernels[0],strides = [1,1,1,1],padding = 'VALID'))
+    layer1 = tf.nn.max_pool(amap,[1,3,3,1],[1,1,1,1],padding = "VALID")
+
+    amap = tf.nn.relu(tf.nn.conv2d(layer1,kernels[1],[1,1,1,1],padding = "VALID"))
+    layer2 = tf.nn.max_pool(amap,[1,3,3,1],[1,1,1,1],'VALID')
+
+    layer3 = tf.nn.relu(tf.nn.conv2d(layer2,kernels[2],[1,1,1,1],'VALID'))
+    layer4 = tf.nn.relu(tf.nn.conv2d(layer3,kernels[3],[1,1,1,1],'VALID'))
+    layer5 = tf.nn.relu(tf.nn.conv2d(layer4,kernels[4],[1,1,1,1],'VALID'))
+
+    out_dims = [batch_size,10,10,256]
+    layer5_flat = tf.reshape(layer5,shape = [batch_size,out_dims[1]*out_dims[2]*out_dims[3]])
+
+    layer1 = tf.relu(tf.matmul(layer5_flat,kernels[5]) + biases[5])
+    layer2 = tf.relu(tf.matmul(layer1, kernels[6]) + biases[6])
+    layer3 = tf.matmul(layer2, kernels[7]) + biases[7]
+
+    soft_max = tf.nn.softmax(layer3)
 
 
 with tf.Session(gph) as sess:
